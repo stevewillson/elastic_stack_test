84a7fa483ebba9843d65a0540cff83b6b4d1f76f
Steve Willson <steve.willson@gmail.com>
2018-06-26 04:38:44 -0400

    add docker install and docker download files

---
+++ download_es_docker_containers.sh
@@ -0,0 +1,11 @@
+#
+# Steve Willson
+# 6/26/18
+# download elasticsearch, kibana, and logstash docker containers
+#
+#
+
+# download Elasticsearch, Kibana, and Logstash images
+docker pull docker.elastic.co/elasticsearch/elasticsearch-oss:6.3.0
+docker pull docker.elastic.co/kibana/kibana-oss:6.3.0
+docker pull docker.elastic.co/logstash/logstash-oss:6.3.0
---
+++ setup_docker.sh
@@ -0,0 +1,36 @@
+#
+# Steve Willson
+# 6/26/18
+# Download docker and install
+#
+
+# remove old versions of docker
+echo "Removing old versions of docker"
+sudo apt-get remove docker docker-engine docker.io
+
+# update the repositories
+sudo apt-get update
+
+#Install packages to allow apt to use a repository over HTTPS:
+sudo apt-get install -y \
+apt-transport-https \
+ca-certificates \
+curl \
+software-properties-common
+
+# add the docker gpg key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# add the docker repo
+sudo add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) \
+stable"
+
+# Update the apt package index.
+sudo apt-get update
+
+# Install the latest version of Docker CE, or go to the next step to install a specific version:
+sudo apt-get install docker-ce -y
+
+
