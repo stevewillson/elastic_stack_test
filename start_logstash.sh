da31ca1204012ed3bfa2bf5a02435165d6ab22ea
Steve Willson <stephen.l.willson.mil@mail.mil>
2018-08-10 11:08:10 -0400

    add startup info to logstash start, reload conf files every 3 seconds

--- start_logstash.sh
+++ start_logstash.sh
@@ -4,5 +4,20 @@
 # script to start logstash
 #
 
-sudo docker run --rm -it -v /home/user/elastic_stack_test/pipeline/:/usr/share/logstash/pipeline/ -v /home/user/elastic_stack_test/data/:/usr/share/logstash/input_data/ -p 9600:9600 --link elastic:es --name logstash docker.elastic.co/logstash/logstash-oss:6.3.0
+###### Docker Options #####
+# --rm -> remove the container after exit
+# -i -> interactive - keep STDIN open even if not attached
+# -t -> allocate a pseudo-TTY
+# -v -> create a bind mount, binds HOST_DIR to CONTAINER_DIR
+# -p -> publish a container's port to the host HOST_PORT:CONTAINER_PORT
+# --link -> add link to another container name_or_id[:alias]
+# --name -> assign a name to the container
+# IMAGE - specify the docker image to run
+
+# COMMAND (not currently used) - overwrites the default entry point of the container (what is run when the container starts)
+
+# TODO: remove explicit path from starting logstash, mapping directories
+
+
+sudo docker run --rm -it -v /home/user/elastic_stack_test/pipeline/:/usr/share/logstash/pipeline/ -v /home/user/elastic_stack_test/data/:/usr/share/logstash/input_data/ -p 9600:9600 -p 9999:9999 --link elastic:es --name logstash docker.elastic.co/logstash/logstash-oss:6.3.0 -r
 
